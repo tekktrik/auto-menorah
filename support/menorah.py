4b5d1ebfb932315b3d94cafdd6ed1bb56a65444e
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-12-04 15:18:52 -0500

    Create static method for getting menorah's off time

--- support/menorah.py
+++ support/menorah.py
@@ -1,3 +1,5 @@
+from adafruit_datetime import timedelta
+
 try:
     import typing  # pylint = disable:unused-import
     from digitalio import DigitalInOut
@@ -14,3 +16,13 @@ class Menorah:
 
         self.shamash = shamash_dio
         self.candles = candles_dio
+
+    @staticmethod
+    def get_menorah_off_time(lighting_time: datetime) -> datetime:
+        """Get the time at while candles should be turned off"""
+        hour_difference: datetime = 29 - lighting_time.hour
+        projected_time: datetime = lighting_time + timedelta(hours=hour_difference)
+        projected_time.minute = 0
+        projected_time.second = 0
+        projected_time.microsecond = 0
+        return projected_time
