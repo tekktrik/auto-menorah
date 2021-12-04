6fab49ff25a43ec2ae5ab55ffec8bd894a6bbe95
Alec Delaney <tekktrik@gmail.com>
2021-12-04 11:33:01 -0500

    Sync time, add recursive JSON getting

--- support/wifi_manager.py
+++ support/wifi_manager.py
@@ -43,8 +43,41 @@ class WiFiManager(ESP_SPIcontrol):
                     print("Could not connect, retrying in 5 seconds...")
                     time.sleep(5)
 
-    def grab_candle_lighting_time(
+    def sync_time(self, num_attempts: int = 5) -> bool:
+        for attempt in range(num_attempts):
+            try:
+                self._ntp_time = super().get_time()[0]
+                return True
+            except ValueError:
+                if attempt != (num_attempts - 1):
+                    print("Failed to sync with NTP server")
+                    print("Trying again in 5 seconds")
+                    time.sleep(5)
+        return False
+
+    def _update_json(self, month):
+        return requests.get(CALENDAR_API.replace("[|MONTH|]", str(month)))["items"]
+
+    def _parse_time_for_night(self, num_night):
+        for event in self._latest_events:
+            if event["title"] == ("Chanukah: " + str(num_night) + " Candle"):
+                return datetime.fromisoformat(event["date"])
+        self._month_checking += 1
+        self._update_json()
+        return self._parse_time_for_night(num_night)
+
+    def get_candle_lighting_time(
         self,
-    ) -> Dict[str, str]:  # TODO: Check and finalize typing
-        # Function to grab data on the Hebrew calendar for the dates and times
-        requests.get()
+    ) -> List[datetime]:  # TODO: Check and finalize typing
+        """Function to grab data on the Hebrew calendar for the dates and times"""
+        self._month_checking = 11
+        lighting_times = []
+
+        # recursion
+
+        self._latest_events = self._update_json()
+
+        for night in range(8):
+            lighting_times.append(self._parse_time_for_night(night))
+
+        return lighting_times
