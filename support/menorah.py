803006197b1ba9cde68c910420a226f58c5f2543
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-12-05 21:25:47 -0500

    Add function to sleep based on time delta

--- support/menorah.py
+++ support/menorah.py
@@ -30,3 +30,16 @@ class Menorah:
         projected_time.second = 0
         projected_time.microsecond = 0
         return projected_time
+
+    @staticmethod
+    def sleep_based_on_delta(lighting_time: datetime, current_time: datetime) -> None:
+        """Sleeps the program for a given amount of time depending on the time delta
+        
+        :param datetime lighting_time: The time at which candles should be lit for that day
+        :param datetime current_time: The current time
+        """
+        time_diff: timedelta = lighting_time - current_time
+        time_diff_s = time_diff.total_seconds()
+        time_to_sleep = 60 if time_diff_s > 60 else time_diff_s
+        if time_to_sleep > 0:
+            time.sleep(time_to_sleep)
