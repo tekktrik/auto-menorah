08359d7d40ae751929eb5ba3068abff9c348709e
Alec Delaney <tekktrik@gmail.com>
2021-12-08 17:32:55 -0500

    Force 15 second sleep
    To fix implementation later

--- support/menorah.py
+++ support/menorah.py
@@ -51,9 +51,10 @@ class Menorah:
         :param datetime lighting_time: The time at which candles should be lit for that day
         :param datetime current_time: The current time
         """
-        time_diff: timedelta = lighting_time - current_time
-        time_diff_s = time_diff.total_seconds()
-        time_to_sleep = 60 if time_diff_s > 60 else time_diff_s
+        #time_diff: timedelta = lighting_time - current_time # TODO: Fix to mix naive and timezone aware
+        #time_diff_s = time_diff.total_seconds()
+        #time_to_sleep = 60 if time_diff_s > 60 else time_diff_s
+        time_to_sleep = 15 # TODO: Fix the god awful code
         if time_to_sleep > 0:
             time.sleep(time_to_sleep)
 
