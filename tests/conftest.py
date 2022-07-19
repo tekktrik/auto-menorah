ad4f3f6d6d47667623a17a5b268d0bc5ea4afc15
Alec Delaney <tekktrik@gmail.com>
2022-07-18 23:11:46 -0400

    Add testing files

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import sys
+import socket
+import importlib
+
+wifi_spec = importlib.machinery.ModuleSpec("wifi", None)
+wifi_module = importlib.util.module_from_spec(wifi_spec)
+
+secrets_spec = importlib.machinery.ModuleSpec("secrets", None)
+secrets_module = importlib.util.module_from_spec(secrets_spec)
+
+sys.modules["wifi"] = wifi_module
+sys.modules["secrets"] = secrets_module
+sys.modules["socketpool"] = socket
+
+def pytest_addoption(parser):
+    parser.addoption("--aio_username", action="store")
+    parser.addoption("--aio_key", action="store")
+    parser.addoption("--location", action="store")
+
+
+def pytest_generate_tests(metafunc):
+
+    aio_username = metafunc.config.option.aio_username
+    aio_key = metafunc.config.option.aio_key
+    location = metafunc.config.option.location
+
+    print("FDSFJNDSJFBSDJ")
+    if "aio_username" in metafunc.fixturenames and aio_username is not None:
+        metafunc.parametrize("aio_username", [aio_username])
+    if "aio_key" in metafunc.fixturenames and aio_key is not None:
+        metafunc.parametrize("aio_key", [aio_key])
+    if "location" in metafunc.fixturenames and location is not None:
+        metafunc.parametrize("location", [location])
---
+++ tests/test_lighting_logic.py
@@ -0,0 +1,61 @@
+import sys
+import os
+
+current_path = os.path.normpath(__file__)
+current_path_comps = current_path.split(os.sep)
+support_path = os.sep + os.path.join(*current_path_comps[:-2])
+sys.path.append(support_path)
+
+import secrets
+secrets.secrets = {
+    "aio_username": None,
+    "aio_key": None,
+}
+secrets.location = {
+    "zipcode": None,
+}
+
+from support import menorah
+from support.menorah import Menorah
+from support import wifi_manager
+from support.wifi_manager import WiFi, TIME_URL
+
+import requests
+import adafruit_datetime
+
+wifi = WiFi()
+wifi.requests = requests
+
+def test_lighting_times(monkeypatch, aio_username, aio_key, location):
+
+    secrets_secrets = {
+        "aio_username": aio_username,
+        "aio_key": aio_key,
+    }
+    secrets_location = {
+        "zipcode": location,
+    }
+    monkeypatch.setattr(wifi_manager, "secrets", secrets_secrets)
+    monkeypatch.setattr(wifi_manager, "location", secrets_location)
+
+    lighting_times = wifi.get_candle_lighting_times()
+    
+    for lighting_time in lighting_times:
+
+        off_time = wifi.get_menorah_off_time(lighting_time)
+
+        assertion_pairs = []
+
+        assertion_pairs.append((lighting_time, lighting_time - adafruit_datetime.timedelta(hours=12), 60))
+        assertion_pairs.append((lighting_time, lighting_time - adafruit_datetime.timedelta(minutes=45), 60))
+        assertion_pairs.append((lighting_time, lighting_time - adafruit_datetime.timedelta(seconds=28), 28))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(seconds=28), 60))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(minutes=45), 60))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(hours=3), 60))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(hours=9), 60))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(hours=11, minutes=15), 60))
+        assertion_pairs.append((off_time, lighting_time + adafruit_datetime.timedelta(hours=11, minutes=59, seconds=25), 35))
+
+        for event_time, mock_time, expected_sleep in assertion_pairs:
+            print("A")
+            assert Menorah.get_sleep_time_based_on_delta(event_time, mock_time) == expected_sleep
