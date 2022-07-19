a0330d18f5b8e6551db5053299e1ac812123e0a8
Alec Delaney <tekktrik@gmail.com>
2022-07-18 23:38:26 -0400

    Linted setup_helper

--- support/setup_helper.py
+++ support/setup_helper.py
@@ -2,7 +2,20 @@
 #
 # SPDX-License-Identifier: MIT
 
+"""
+`setup_helper`
+==============
 
+Simple module for holding the connectin status
+
+* Author(s): Alec Delaney
+
+"""
+
+
+# pylint: disable=too-few-public-methods
 class ConnectionStatus:
+    """The connection status"""
+
     def __init__(self):
         self.is_connected = False
